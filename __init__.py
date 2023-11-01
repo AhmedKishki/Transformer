a603e4a920170bae28b804ebe403f94d37961552
kishki <alaa6394@gmail.com>
2023-11-01 19:41:47 +0100

    new

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from Transformer2.helpers import *
+from Transformer2.layer import LayerNorm, SublayerConnection, Generator
+from Transformer2.attention import MultiHeadedAttention
+from Transformer2.feedforward import FeedForward
+from Transformer2.embeddings import Embeddings, PositionalEncoding
+from Transformer2.transformer import Transformer
+from Transformer2.encoder import Encoder, EncoderLayer
+from Transformer2.decoder import Decoder, DecoderLayer
+from build import make_model
---
+++ feedforward.py
@@ -0,0 +1,12 @@
+from torch import nn
+
+class FeedForward(nn.Module):
+    "Implements FFN equation."
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(FeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
