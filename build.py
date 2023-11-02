1d7ed5ed049cae92a20cfa37b5a2e57c5839cbaa
kishki <alaa6394@gmail.com>
2023-11-02 01:17:44 +0100

    updates

--- build.py
+++ build.py
@@ -1,14 +1,12 @@
-import torch
-from torch import nn
-from attention import MultiHeadedAttention
 import copy
-from layer import Generator
+from torch import nn
+from helpers import *
+from attention import MultiHeadedAttention
 from embeddings import Embeddings, PositionalEncoding
 from feedforward import FeedForward
-from helpers import *
-from transformer import Transformer
 from encoder import Encoder, EncoderLayer
 from decoder import Decoder, DecoderLayer
+from architecture import Transformer, Generator
 
 def make_model(
     src_vocab, 
@@ -39,33 +37,3 @@ def make_model(
         if p.dim() > 1:
             nn.init.xavier_uniform_(p)
     return model
-
-def inference_test():
-    test_model = make_model(11, 11, 2)
-    test_model.eval()
-    src = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-    src_mask = torch.ones(1, 1, 10)
-
-    memory = test_model.encode(src, src_mask)
-    ys = torch.zeros(1, 1).type_as(src)
-
-    for i in range(9):
-        out = test_model.decode(
-            memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data)
-        )
-        prob = test_model.generator(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.data[0]
-        ys = torch.cat(
-            [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
-        )
-
-    print("Example Untrained Model Prediction:", ys)
-
-
-def run_tests():
-    for _ in range(10):
-        inference_test()
-        
-if __name__ == "__main__":
-    run_tests()
