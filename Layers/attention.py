ef711a5aab66953c6776b0324c3aec7da663a146
kishki <alaa6394@gmail.com>
2023-11-07 00:04:22 +0100

    new

--- Layers/attention.py
+++ Layers/attention.py
@@ -7,7 +7,7 @@ def attention(query, key, value, mask=None, dropout=None):
     d_k = query.size(-1)
     scores = torch.matmul(query, key.transpose(-2, -1)) / sqrt(d_k)
     if mask is not None:
-        scores = scores.masked_fill(mask == 0, -1e9)
+        scores = scores.masked_fill(mask == 0, float('-inf'))
     p_attn = scores.softmax(dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
@@ -21,35 +21,30 @@ class MultiHeadedAttention(nn.Module):
         # We assume d_v always equals d_k
         self.d_k = d_model // h
         self.h = h
-        self.linears = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(4)])
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.proj = nn.Linear(d_model, d_model)
         self.attn = None
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, query, key, value, mask=None):
-        "Implements Figure 2"
         if mask is not None:
             # Same mask applied to all h heads.
             mask = mask.unsqueeze(1)
         nbatches = query.size(0)
 
         # 1) All the linear projections in batch from d_model => h x d_k
-        query, key, value = [
-            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
-            for lin, x in zip(self.linears, (query, key, value))
-        ]
+        query = self.w_q(query).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+        key =self.w_k(key).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+        value = self.w_v(value).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
 
         # 2) Apply attention on all the projected vectors in batch.
-        x, self.attn = attention(
-            query, key, value, mask=mask, dropout=self.dropout
-        )
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
 
         # 3) "Concat" using a view and apply a final linear.
-        x = (
-            x.transpose(1, 2)
-            .contiguous()
-            .view(nbatches, -1, self.h * self.d_k)
-        )
+        x = (x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k))
         del query
         del key
         del value
-        return self.linears[-1](x)
+        return self.proj(x)
--- Layers/decoder.py
+++ Layers/decoder.py
@@ -7,18 +7,18 @@ c = copy.deepcopy
 class DecoderLayer(nn.Module):
     "Decoder is made of self-attn, src-attn, and feed forward (defined below)"
 
-    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+    def __init__(self, size, self_attn, cross_attn, feed_forward, dropout):
         super(DecoderLayer, self).__init__()
         self.size = size
-        self.self_attn = self_attn
-        self.src_attn = src_attn
+        self.s_attn = self_attn
+        self.x_attn = cross_attn
         self.feed_forward = feed_forward
         self.sublayer = nn.ModuleList([c(SublayerConnection(size, dropout)) for _ in range(3)])
 
     def forward(self, x, memory, src_mask, tgt_mask):
         m = memory
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
-        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        x = self.sublayer[0](x, lambda x: self.s_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.x_attn(x, m, m, src_mask))
         return self.sublayer[2](x, self.feed_forward)
     
 class Decoder(nn.Module):
